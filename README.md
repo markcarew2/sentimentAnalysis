3677fe382014ca436517676c227075a102de492f
Mark Carew <markycarew@hotmail.com>
2021-02-04 22:43:38 -0500

    Chose Better Parameters

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ There are 25,000 positive and 25,000 negative reviews.
 It uses the basic SKLearn stochastic gradient descent classifier.
 
 I reduced the features based on the chiSquare information gain. Keeping the top 1% approximately. This attains about the same to a slightly higher test score and runs in about a third of the time in practice.
+
+Changed the loss function to hinge and commented out everything but the bigram classifiers.
+
+Also tweaked the parameters to minimize bias and variance. Increased maxIter to 10000 and increased the regularization paramater (alpha) to .01.
+
+Still only at 89% test accuracy (98% training accuracy). I believe the problem is now high variance but this stems from having too little data or improperly processed data. Increasing the regularization parameter further brings the train and test score closer together but lowers the test score. 
+
+Could probably improve the data used. Used chisquare to reduce vocabulary but there are likely better, domain specific ways of choosing which words to consider.
--- driver.py
+++ driver.py
@@ -18,12 +18,18 @@ testData = pd.read_csv("imdb_te.csv", index_col=0)
 #Want to measure this time to add to the Reduced Unigram Time 
 #get approx time it would take reduced Unigram to do this without having to run twice
 unigramConstructTimeStart = time.time()
+"""
 unigramVectorizer = CountVectorizer()
 unigramVector = unigramVectorizer.fit_transform(df["text"])
 clfUnigram = SGDClassifier(loss="log", penalty="l1")
+"""
 unigramConstructTime = time.time() - unigramConstructTimeStart
-
+"""
 clfUnigram.fit(unigramVector, y = df["polarity"])
+trainScore1 = clfUnigram.predict(unigramVector)
+trainScore = metrics.accuracy_score(df["polarity"], trainScore1)
+print("Not reduced Unigram Train Score: ", trainScore)
+
 testVector = unigramVectorizer.transform(testData["text"])
 pred = clfUnigram.predict(testVector)
 
@@ -32,35 +38,54 @@ F1Score = metrics.f1_score(testData["polarity"], pred, average="macro")
 
 print("NonReduced Unigram Accuracy/F1: ", acc)
 print(F1Score)
+"""
 unigramTime = time.time() - startTime
+
+"""
 print("Unigram Time = %s seconds" % unigramTime)
 print("*****")
+
 
 #Reduced Unigram
 selectBest = SelectKBest(chi2, k =1000)
 unigramVectorReduce = selectBest.fit_transform(unigramVector, df["polarity"])
 
 clfUnigram.fit(unigramVectorReduce, y = df["polarity"])
+
+trainScore1 = clfUnigram.predict(unigramVectorReduce)
+trainScore = metrics.accuracy_score(df["polarity"], trainScore1)
+print("Reduced Unigram Training Accuracy: ", trainScore)
+
 testVectorReduce = selectBest.transform(testVector)
 pred = clfUnigram.predict(testVectorReduce)
 acc = metrics.accuracy_score(testData["polarity"], pred)
 F1Score = metrics.f1_score(testData["polarity"], pred, average="macro")
 
+
 print("Reduced Unigram Accuracy/F1: ", acc)
 print(F1Score)
+"""
 unigramReducedTime = time.time() - unigramTime - startTime + unigramConstructTime
+
+"""
 print("Unigram Reduced Time = %s seconds" % unigramReducedTime)
 print("*****")
+"""
 
 
 #Try again with Bigrams, reduce to about 1%
 bigramConstructTimeStart = time.time()
 unibigramVectorizer = CountVectorizer(ngram_range=(1,2))
 bigramVector = unibigramVectorizer.fit_transform(df["text"], y = df["polarity"])
-clfBigram = SGDClassifier(loss="log", penalty="l1")
+clfBigram = SGDClassifier(loss="hinge", penalty="l2", alpha=.01, max_iter=10000)
 bigramConstructTime = time.time() - bigramConstructTimeStart
 
 clfBigram.fit(bigramVector,y = df["polarity"])
+
+trainScore1 = clfBigram.predict(bigramVector)
+trainScore = metrics.accuracy_score(df["polarity"], trainScore1)
+print("NonReduced Bigram Training Accuracy: ", trainScore)
+
 
 testVector = unibigramVectorizer.transform(testData["text"])
 
@@ -71,13 +96,18 @@ F1Score = metrics.f1_score(testData["polarity"], pred, average="macro")
 print("Bigram NonReduced Accuracy/F1: " ,acc)
 print(F1Score)
 bigramTime = time.time() - unigramTime - unigramReducedTime - startTime
-print("Bigram Time = %s seconds" % bigramTime)
+#print("Bigram Time = %s seconds" % bigramTime)
 print("******")
 
 #Reduced Bigram
 selectBestBigram = SelectKBest(chi2, k =15138)
 bigramVectorReduce = selectBestBigram.fit_transform(bigramVector, df["polarity"])
 clfBigram.fit(bigramVectorReduce,y = df["polarity"])
+
+trainScore1 = clfBigram.predict(bigramVectorReduce)
+trainScore = metrics.accuracy_score(df["polarity"], trainScore1)
+print("Reduced Bigram Training Accuracy: ", trainScore)
+
 
 testVectorReduce = selectBestBigram.transform(testVector)
 
@@ -88,4 +118,4 @@ F1Score = metrics.f1_score(testData["polarity"], pred, average="macro")
 print("Bigram Reduced Accuracy/F1: " ,acc)
 print(F1Score)
 bigramReducedTime = time.time() - bigramTime - unigramTime - unigramReducedTime - startTime + bigramConstructTime
-print("Bigram Reduced Time = %s seconds" % bigramReducedTime)
+#print("Bigram Reduced Time = %s seconds" % bigramReducedTime)
